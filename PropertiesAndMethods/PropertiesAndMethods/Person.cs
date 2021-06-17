cf1d4f16ff93c461992d5df73c9ae06b2e5204bf
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-06-17 19:11:25 +0300

    Example with a simple property

---
+++ PropertiesAndMethods/PropertiesAndMethods/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAndMethods
+{
+    public class Person
+    {
+        private string firstName;
+        
+        public string FirstName
+        {
+            get 
+            {
+                /* executed when someone tries to read FirstName */
+                Console.WriteLine("Executing FirstName.get");
+                if (string.IsNullOrEmpty(firstName))
+                {
+                    firstName = "Test FirstName";
+                }
+
+                return firstName;
+            }
+            set
+            {
+                /* executed when someone tries to write FirstName */
+                Console.WriteLine("Executing FirstName.set");
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    value = string.Empty;
+                }
+
+                firstName = value;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Hello, my name is {FirstName}");
+        }
+    }
+}
---
+++ PropertiesAndMethods/PropertiesAndMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PropertiesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.FirstName = "Abcd";
+            p.Print();
+        }
+    }
+}
